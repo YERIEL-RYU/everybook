84f843f81390445e60b2429252cdaa4eeca2ca82
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-06-09 14:03:15 +0900

    [EveryBook] 상벌점 기준
     file : officePointSandard.html, PointController.java
     detail : 직원 페이지 상벌점기준 리스트, 등록, 수정, 삭제 폼 생성
     		  controller로 화면

--- teamproject/src/main/java/team1/project/controller/PointController.java
+++ teamproject/src/main/java/team1/project/controller/PointController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PointController {
 	
+	@GetMapping("/officePointSandard")
+	public String officePointSandard() {
+		return "point/officePointSandard";
+	}
+	
 	@GetMapping("/myPoint")
 	public String myPoint() {
 		return "point/myPoint";
