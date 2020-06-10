e438cce17caf9493f5fc34c27fefeb648c30a5bb
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-06-10 15:22:16 +0900

    [EveryBook] 직원 예약 리스트화면, 등록 화면, 수정화면, 삭제화면 생성 			직원 도서관 리스트, 등록화면, 수정화면, 삭제화면 생성 			이용안내 메뉴 시작 준비
     file : LibraryController.java
     detail : class 이름 변경
    
     file : libraryList.html
     detail : 이용안내 메뉴 준비
    
     file : officeLibrary.html
     detail : 직원 도서관 리스트 (등록 수정 삭제)
    
     file : officeRtcanReserveAdd.html, officeRtcanReserveList.html,
    officeRtingReserve.html ReserveController.java
     detail : 직원 예약 관리 생성 - 온라인 (완료) 오프라인 (폼 생성 필요)
    
     file : officePointHistoryAdd.html, officePointHistoryList.html
     detail : 테이블 스타일 변경 (가운데 정렬)

--- teamproject/src/main/java/team1/project/controller/LibraryController.java
+++ teamproject/src/main/java/team1/project/controller/LibraryController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Library {
+public class LibraryController {
+	
+	@GetMapping("/officeLibrary")
+	public String getOfficeLibraryList() {
+		return "library/officeLibrary";
+	}
 	
 	@GetMapping("/officeRegion")
-	public String getReionList() {
+	public String getOfficeReionList() {
 		return "library/officeRegion";
 	}
 }
--- teamproject/src/main/java/team1/project/controller/ReserveController.java
+++ teamproject/src/main/java/team1/project/controller/ReserveController.java
@@ -6,6 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ReserveController {
 	
+	@GetMapping("/officeRtcanReserveList")
+	public String rtcanReserveList() {
+		return "reserve/officeRtcanReserveList.html";
+	}
+	@GetMapping("/officeRtcanReserveAdd")
+	public String rtcanReserveAdd() {
+		return "reserve/officeRtcanReserveAdd.html";
+	}
+	
 	@GetMapping("/myReserve")
 	public String myReserve() {
 		return "reserve/myReserve";
