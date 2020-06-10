aeb0c432768016ce337c31f6c82368d27a037f1f
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-06-10 16:35:56 +0900

    [EveryBook] 도서관 목록 작업 중
     file : libraryList.html, MainController.java
     detail : 이용안내 메뉴 중 도서관 목록 작업 중 -> 검색 폼 style 작업 중

--- teamproject/src/main/java/team1/project/controller/MainController.java
+++ teamproject/src/main/java/team1/project/controller/MainController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
+	@GetMapping("/libraryList")
+	public String getLibraryList() {
+		return "information/libraryList";
+	}
+	
 	@GetMapping("/office")
 	public String admin() {
 		return "admin/indexAdmin";
