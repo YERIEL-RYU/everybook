e5f004917e2876ec570dcf281f6ed242296e92b2
SSR <tjtkfkd12@gmail.com>
2020-06-13 17:42:59 +0900

    [EveryBook] 나의 대여 내역 폼 생성, RentController 수정, ReturnController 수정 			희망도서신청 폼 수정
     file : addBookApply.html
     , myRent.html
     , RentController.java
     , ReturnController.java
     detail : 나의대여내역 레이아웃 구현, 컨트롤러 맵핑 추가, Return컨트롤러 이름수정, 희망도서신청
     버튼 위치 수정

--- teamproject/src/main/java/team1/project/controller/RentController.java
+++ teamproject/src/main/java/team1/project/controller/RentController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RentController {
 
+	@GetMapping("/myRent")
+	public String myRent() {
+		return "rent/myRent";
+	}
+	
 	@GetMapping("/officeBookRent")
 	public String officeRent() {
 		return "rent/officeBookRent";
