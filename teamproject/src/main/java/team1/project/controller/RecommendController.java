b3600e64acb9ee8f7d012c07abecb2517f49d02f
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-06-14 22:32:24 +0900

    [EveryBook] 가로스크롤 추가 및 인기도서 리스트
    file : index.html
    detail : 인기도서 추천도서 가로 스크롤 추가
    
    file : RecommendController.java, recommendList.html, vogueBookList.html
    detail : 화면 구성

--- teamproject/src/main/java/team1/project/controller/RecommendController.java
+++ teamproject/src/main/java/team1/project/controller/RecommendController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RecommendController {
 	
+	@GetMapping("/vogueBookList")
+	public String getVogueBookList() {
+		return "recommend/vogueBookList";
+	}
+	
 	@GetMapping("/recommendList")
 	public String getRecommendList() {
 		return "recommend/recommendList";
