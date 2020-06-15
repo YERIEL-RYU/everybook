268f5bc5ff2ba9d2c1f724fdf44d9dc02bb718d0
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-06-15 10:30:10 +0900

    [EveryBook] 패키지 추가

---
+++ teamproject/src/main/java/team1/project/vo/Region.java
@@ -0,0 +1,43 @@
+package team1.project.vo;
+
+import java.util.Date;
+
+public class Region {
+	private String regionCode;
+	private String regionMajor;
+	private String regionMinor;
+	private Date regionRegDate;
+	private String officer;
+	public String getRegionCode() {
+		return regionCode;
+	}
+	public void setRegionCode(String regionCode) {
+		this.regionCode = regionCode;
+	}
+	public String getRegionMajor() {
+		return regionMajor;
+	}
+	public void setRegionMajor(String regionMajor) {
+		this.regionMajor = regionMajor;
+	}
+	public String getRegionMinor() {
+		return regionMinor;
+	}
+	public void setRegionMinor(String regionMinor) {
+		this.regionMinor = regionMinor;
+	}
+	public Date getRegionRegDate() {
+		return regionRegDate;
+	}
+	public void setRegionRegDate(Date regionRegDate) {
+		this.regionRegDate = regionRegDate;
+	}
+	public String getOfficer() {
+		return officer;
+	}
+	public void setOfficer(String officer) {
+		this.officer = officer;
+	}
+	
+	
+}
