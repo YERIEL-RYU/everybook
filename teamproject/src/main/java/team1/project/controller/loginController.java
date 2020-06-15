70eaeea32ac82dc5bd2eb1141290b1b81dbb9f64
kej-2356 <kej_2356@naver.com>
2020-06-15 17:30:52 +0900

    [EveryBook] 로그인화면 수정
    file : loginController.java,addMember.html,login.html
    detail : 로그인처리 수정

--- teamproject/src/main/java/team1/project/controller/loginController.java
+++ teamproject/src/main/java/team1/project/controller/loginController.java
@@ -1,8 +1,13 @@
 package team1.project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class loginController {
 	
+	@PostMapping("loginMember")
+	public String loginMember() {
+		return "index";
+	}
 }
