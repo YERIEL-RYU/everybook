7f2c7a37a852ea6b687839a3b8c82d9097f2541f
glemfk1111 <tjtkfkd12@gmail.com>
2020-06-18 11:20:28 +0900

    [EveryBook] 오타수정, 제목수정
     file : ExtendController.java
     ,bookDetail.html
     ,bookList.html
     ,officeBooklist.html
     ,addExtend.html
     detail : 연장부분, 책리스트 오타,제목 수정

--- teamproject/src/main/java/team1/project/controller/ExtendController.java
+++ teamproject/src/main/java/team1/project/controller/ExtendController.java
@@ -4,9 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ExtendsController{
+public class ExtendController {
+	
 	@GetMapping("/officeBookextend")
 	public String officeBookextend() {
+		
 		return "extend/officeBookextend";
+	}
+	
+	@GetMapping("/addExtend")
+	public String addExpand() {
+		
+		return "extend/addExtend";
 	}
 }
