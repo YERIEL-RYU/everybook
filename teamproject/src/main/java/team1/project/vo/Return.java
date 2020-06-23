9d63ced178b38f38b049d1d13611b6846495fe69
glemfk1111 <tjtkfkd12@gmail.com>
2020-06-23 11:38:15 +0900

    [EveryBook] 반납도서리스트 조회
    file : ReturnController.java
    , ReturnMapper.java
    , ReturnService.java
    , Return.java
    , Return.xml
    , officeBookReturn.html
    
    detail : 반납도서리스트 조회 기능구현!

--- teamproject/src/main/java/team1/project/vo/Return.java
+++ teamproject/src/main/java/team1/project/vo/Return.java
@@ -7,11 +7,34 @@ public class Return {
 	private String returnCode;
 	private String library;
 	private String member;
-	private String book;
+	private Book book;
 	private String rent;
 	private String returnDate;
 	private Date returnRegDate;
 	private String officer;
+	private String writer;
+	private String category;
+	private String publisher;
+	
+	
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
 	public String getReturnCode() {
 		return returnCode;
 	}
@@ -30,10 +53,10 @@ public class Return {
 	public void setMember(String member) {
 		this.member = member;
 	}
-	public String getBook() {
+	public Book getBook() {
 		return book;
 	}
-	public void setBook(String book) {
+	public void setBook(Book book) {
 		this.book = book;
 	}
 	public String getRent() {
@@ -64,7 +87,7 @@ public class Return {
 	public String toString() {
 		return "Return [returnCode=" + returnCode + ", library=" + library + ", member=" + member + ", book=" + book
 				+ ", rent=" + rent + ", returnDate=" + returnDate + ", returnRegDate=" + returnRegDate + ", officer="
-				+ officer + "]";
+				+ officer + ", writer=" + writer + ", category=" + category + ", publisher=" + publisher + "]";
 	}
 	
 	
