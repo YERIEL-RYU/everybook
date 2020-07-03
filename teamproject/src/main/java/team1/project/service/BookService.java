a29a79a0ed0b52415933dd154bf64b94ed3c6896
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-07-03 10:45:07 +0900

    [EveryBook] officeAddbook의 도서관 정보나루, 국립 중앙 도서관 api 연동
    file : officeAddbook.html, BookService.java, pom.xml, no_image.gif
    detail : isbn 코드로 도서 정보 호출하는 api 연동, jsoup 라이브러리 추가, 책 이미지 없을때 사용할 이미지
    추가

--- teamproject/src/main/java/team1/project/service/BookService.java
+++ teamproject/src/main/java/team1/project/service/BookService.java
@@ -1,7 +1,10 @@
 package team1.project.service;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +23,40 @@ public class BookService {
 	
 	//isbn으로 도서 정보 가져오기 api - 영주
 	public Book searchIsbn(String isbn) {
+		logger.info("BookService - isbn : {}",isbn);
 		String seojiUrl = "http://seoji.nl.go.kr/landingPage/SearchApi.do?"
 					+ "cert_key=028d34c6eb6f18a4cade4731aa6dc4651d41706a33d3ffe5babd55542c96a9f0&"	
-					+ "result_style=json&page_no=1&page_size=10&isbn=";
-		String dataUrl = "http://data4library.kr/api/srchDtlList?"
+					+ "result_style=xml&page_no=1&page_size=10&isbn="+isbn;
+		String naruUrl = "http://data4library.kr/api/srchDtlList?"
 				+ "authKey=d30acf5969608e57f856c36ddd03e661e138d851efb484a2ddffc33ce70627d4&isbn13="
-				+ isbn +"&format=json";
+				+ isbn;
 		
-		return null;
+		Book book = new Book();
+		try {
+			//도서관 정보나루 api
+			Document naru = Jsoup.connect(naruUrl).data("isbn",isbn).get();
+			Document seoji = Jsoup.connect(seojiUrl).data("isbn",isbn).get();
+			String bookName = seoji.select("TITLE").text();
+			String bookPrice = seoji.select("REAL_PRICE").text();
+			String writer = naru.select("authors").text();
+			String publisher = naru.select("publisher").text();
+			String bookDescription = naru.select("description").text();
+			String bookImageURL = naru.select("bookImageURL").text();
+			String category = naru.select("class_no").text();
+			book.setBookName(bookName);
+			book.setWriter(writer);
+			book.setBookDescription(bookDescription);
+			book.setBookImageURL(bookImageURL);
+			book.setPublisher(publisher);
+			book.setBookPrice(bookPrice);
+			book.setCategory(category);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		return book;
 	}
 	
 	//도서 코드로 도서 정보 검색 -영주
