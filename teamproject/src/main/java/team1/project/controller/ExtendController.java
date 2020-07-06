306ccf8660fac1d20fe59144ff7917c198f3e095
glemfk1111 <tjtkfkd12@gmail.com>
2020-07-06 17:32:16 +0900

    [EveryBook] 연장신청 구현 중

--- teamproject/src/main/java/team1/project/controller/ExtendController.java
+++ teamproject/src/main/java/team1/project/controller/ExtendController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import team1.project.service.ExtendService;
 import team1.project.vo.Rent;
@@ -15,13 +17,17 @@ import team1.project.vo.Rent;
 @Controller
 public class ExtendController {
 	@Autowired private ExtendService extendService;
-	
-	@GetMapping("/officeBookextend")
-	public String officeBookextend() {
+
+	//연장신청
+	@PostMapping("/addBookExtend")
+	public String addBookExtend(Rent rent
+							,@RequestParam(name = "getReturnDate") String rentReturnDate) {
+		System.out.println("rentReturnDate --> " + rentReturnDate);
+		return "redirect:/addExtend";
 		
-		return "extend/officeBookextend";
 	}
 	
+	//연장신청 화면
 	@GetMapping("/addExtend")
 	public String myRentList(Model model, HttpSession session) {
 		String SID = (String) session.getAttribute("SID");
