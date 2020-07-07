f49f85a56cf1ec0bc7a6367dc57b1fcaff88480f
glemfk1111 <tjtkfkd12@gmail.com>
2020-07-07 12:18:15 +0900

    [EveryBook] 연장신청 기능 구현
    file : ExtendController.java
    , ExtendMapper.java
    , ExtendService.java
    , ExtendMapper.xml
    , addExtend.html

--- teamproject/src/main/java/team1/project/controller/ExtendController.java
+++ teamproject/src/main/java/team1/project/controller/ExtendController.java
@@ -21,8 +21,11 @@ public class ExtendController {
 	//연장신청
 	@PostMapping("/addBookExtend")
 	public String addBookExtend(Rent rent
-							,@RequestParam(name = "getReturnDate") String rentReturnDate) {
-		System.out.println("rentReturnDate --> " + rentReturnDate);
+							,@RequestParam(name = "getRentCode") String rentCode) {
+		System.out.println("rentCode --> " + rentCode);
+		int i = extendService.myRentExtend(rentCode);
+		System.out.println("실행결과 : " + i);
+
 		return "redirect:/addExtend";
 		
 	}
@@ -32,7 +35,10 @@ public class ExtendController {
 	public String myRentList(Model model, HttpSession session) {
 		String SID = (String) session.getAttribute("SID");
 		System.out.println(SID + " <--SID");
-		
+		String SLEVEL = (String) session.getAttribute("SLEVEL");
+		System.out.println(SLEVEL + " <--SLEVEL");
+		model.addAttribute("SLEVEL", SLEVEL);
+
 		List<Rent> list = extendService.mtRengList(SID); 
 		model.addAttribute("myRentList", list);
 		return "extend/addExtend";
